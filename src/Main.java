69dbcb63326eacc43999e1b07b6f767480f51e6f
Matheus <matheus.hssousa@outlook.com>
2026-03-27 15:57:01 -0300

    Estudo da página 49

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+void main() {
+    Livro livro = new Livro();
+
+    livro.nome = "Java 8 Prático";
+    livro.descricao = "Novos recurso da linguagem";
+    livro.valor = 59.90;
+    livro.isbn = "978-85-66250-46-6";
+
+    livro.mostrarDetalhes();
+
+
+}
